ed02c1ac1d4340ff653e335bce9535ead0477798
Zenithcoder <awonusiolajide@yahoo.com>
2020-05-28 11:41:47 +0100

    enabled migration

--- src/PayrollAPI/Startup.cs
+++ src/PayrollAPI/Startup.cs
@@ -37,12 +37,12 @@ namespace PayrollAPI
         {
             services.AddControllers();
             services.AddCors();
-           services.AddDbContext<DataContext>(x =>  x.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
+          // services.AddDbContext<DataContext>(x =>  x.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
          
-        //    services.AddDbContext<DataContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection2")));     
+            services.AddDbContext<DataContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection2")));     
 
 // Automatically perform database migration
-//services.BuildServiceProvider().GetService<DataContext>().Database.Migrate();
+services.BuildServiceProvider().GetService<DataContext>().Database.Migrate();
            // Register the Swagger generator, defining 1 or more Swagger documents
          /*   services.AddSwaggerGen(c =>
     {
